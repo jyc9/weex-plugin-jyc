fc946480b287e6c16e454806d1d15222a395d041
yaochenfeng <282696845@qq.com>
2019-04-15 15:36:20 +0800

    项目文件依赖更新

---
+++ ios/Sources/weex/BHWeexURLActionRequestMiddleware.h
@@ -0,0 +1,16 @@
+//
+//  BHWeexURLActionRequestMiddleware.h
+//  Pods
+//
+//  Created by 姚晨峰 on 2019/4/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BHWeexURLActionRequestMiddleware : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Sources/weex/BHWeexURLActionRequestMiddleware.m
@@ -0,0 +1,12 @@
+//
+//  BHWeexURLActionRequestMiddleware.m
+//  Pods
+//
+//  Created by 姚晨峰 on 2019/4/15.
+//
+
+#import "BHWeexURLActionRequestMiddleware.h"
+
+@implementation BHWeexURLActionRequestMiddleware
+
+@end
