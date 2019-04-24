823c990d3f0a16d661b75ed027c592a44e7a9b39
yaochenfeng <282696845@qq.com>
2019-04-24 10:22:18 +0800

    添加本地渲染

--- playground/ios/WeexDemo/WXTabbarViewController.m
+++ playground/ios/WeexDemo/WXTabbarViewController.m
@@ -24,6 +24,16 @@
             [arrs addObject:[[BHNavigationController alloc] initWithRootViewController:vc]];
         }
     }
+    NSString *bundlejs = [NSString stringWithFormat:@"file://%@/bundlejs/dist/pages/index.js?wh_weex=true",[NSBundle mainBundle].bundlePath];
+    NSURL *home = [NSURL URLWithString:bundlejs];
+    {
+        UIViewController *tripvc = [[BHShareKit sharedKit] viewControllerForURL:home];
+        if(tripvc){
+            tripvc.title = @"home";
+            tripvc.tabBarItem.title = @"weexhome";
+            [arrs addObject:[[BHNavigationController alloc] initWithRootViewController:tripvc]];
+        }
+    }
     UIViewController *tripvc = [[BHShareKit sharedKit] viewControllerForURL:[NSURL URLWithString:@"https://h5.m.taobao.com/trip/weex-ui/index.html?_wx_tpl=https%3A%2F%2Fh5.m.taobao.com%2Ftrip%2Fweex-ui%2Fdemo%2Findex.native-min.js"]];
     if(tripvc){
         tripvc.title = @"weex";
